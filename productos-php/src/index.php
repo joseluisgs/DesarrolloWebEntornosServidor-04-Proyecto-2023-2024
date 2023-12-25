ea38254593dee9381b08475f7abcb29e5138704f
José Luis González Sánchez <joseluisgonsan@gmail.com>
2023-12-25 13:54:17 +0100

    Esqueleto init

--- productos-php/src/index.php
+++ productos-php/src/index.php
@@ -2,17 +2,28 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>My PHP App</title>
+    <title>Productos</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+          integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link href="/images/favicon.png" rel="icon" type="image/png">
 </head>
 <body>
-<h1>Bienvenidos/as a mi App con PHP</h1>
-<p>Entorno de PHP usando Docker</p>
+<div class="container">
+    <?php
+    require_once 'header.php';
+    ?>
+
+</div>
 
 <?php
-echo "Hola 2ºDAW!";
-phpinfo();
+require_once 'footer.php';
 ?>
 
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+        crossorigin="anonymous"></script>
 </body>
 </html>
