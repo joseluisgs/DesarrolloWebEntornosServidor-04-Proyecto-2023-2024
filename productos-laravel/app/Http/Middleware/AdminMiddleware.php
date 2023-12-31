e6fc5f611d1800c283f23c179d3f5440803152e5
José Luis González Sánchez <joseluisgonsan@gmail.com>
2023-12-31 11:22:22 +0100

    Middleware admin

---
+++ productos-laravel/app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->role === 'admin') {
+            return $next($request);
+        }
+
+        // Si el usuario no es administrador, puedes redirigirlo a donde prefieras
+        return redirect('home')->with('error', 'No tienes acceso a esta sección');
+    }
+}
